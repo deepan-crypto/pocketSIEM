326cda9fe42f4fb68fc7b981ad8548c251350880
deepan-crypto <deepang467@gmail.com>
2025-12-25 15:19:28 +0530

    main.java

---
+++ threat-intelligence-relay/src/main/java/com/pocketsiem/PocketSiemApplication.java
@@ -0,0 +1,12 @@
+package com.pocketsiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketSiemApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(PocketSiemApplication.class, args);
+    }
+}
